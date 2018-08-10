e1f597df6501a6b271cce0cabc5500a40aaf8429
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-08-10 14:56:11 -0700

    some styling

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Route path="/" component={Header} />
         <Route exact path="/" component={Main} />
         <Route path="/login" component={Login} />
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,57 +1,9 @@
 import React from 'react';
-import { Button } from 'reactstrap';
 
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loggedin: false,
-    };
-  }
-
-  componentDidMount() {
-    if (localStorage.getItem('jwt')) {
-      this.setState({ loggedin: true });
-    }
-  }
-
-  componentWillReceiveProps() {
-    if (localStorage.getItem('jwt')) {
-      this.setState({ loggedin: true });
-    } else {
-      this.setState({ loggedin: false });
-    }
-  }
-
-  handleJokeClick = () => {
-    this.props.history.push('/jokes');
-  };
-
-  handleLoginClick = () => {
-    this.props.history.push('/login');
-  };
-
-  handleRegisterClick = () => {
-    this.props.history.push('/signup');
-  };
-
-  render() {
-    return (
-      <div>
-        Welcome to Dad Jokes!!!
-        <br />
-        <br />
-        {this.state.loggedin ? (
-          <Button onClick={this.handleJokeClick}>Jokes</Button>
-        ) : (
-          <div>
-            <Button onClick={this.handleLoginClick}>Log in</Button>
-            <Button onClick={this.handleRegisterClick}>Regsiter</Button>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+const Main = () => (
+  <div>
+    <h1>Welcome to Dad Jokes!!!</h1>
+  </div>
+);
 
 export default Main;
